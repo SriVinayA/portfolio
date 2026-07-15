5995ceea628b75af65e1b065b7b85a636edc0c1c
SriVinayA <srivinay.appari@gmail.com>
2026-07-15 01:22:49 -0400

    feat: add ChatRequest DTO

---
+++ src/main/java/com/vinayappari/portfolio_backend/dto/ChatRequest.java
@@ -0,0 +1,8 @@
+package com.vinayappari.portfolio_backend.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record ChatRequest(
+    @NotBlank(message = "Message cannot be blank") 
+    String message
+) {}
